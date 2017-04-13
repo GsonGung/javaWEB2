ef2c22dd03368b27b66839a57cef6ac171abb825
gongjd <gjd0564@vip.qq.com>
2017-04-13 21:52:07 +0800

    添加websocket例子

---
+++ Spring-Boot-Demo/src/main/java/cn/mrdear/controller/ViewController.java
@@ -0,0 +1,17 @@
+package cn.mrdear.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Administrator on 2017/4/13.
+ */
+@Configuration
+public class ViewController extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/ws").setViewName("/ws");
+    }
+}
